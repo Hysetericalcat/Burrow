6451369bb44ff6845371ee1b3ea69520f4712760
Hysetericalcat <akshat124b@gmail.com>
2026-05-02 09:27:14 +0530

    fixes

--- packages/server/server.ts
+++ packages/server/server.ts
@@ -6,6 +6,7 @@ app.use(express.json())
 app.post("/query",async(req:Request,res:Response)=>{
    const data = req.body
    console.log(data.message)
+   res.send(data.message)
 })
 
 console.log("Server is listening on port 3000")
